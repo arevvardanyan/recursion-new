6f5cb6eabf5d0870598a5e840f19adc67cab781b
Arev <vardanyanarevik23@gmail.com>
2023-09-11 11:52:55 +0400

    ex-2 fibonacci using recursion

--- resursion.js
+++ resursion.js
@@ -1,12 +1,12 @@
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// 1
-
-const factorial = function(a,fac = 0){
-    if(a === 0){
-        return 1
+// 2
+// 1,1,2,3,5,8,13,21
+const fibonacci = function(a){
+    if(a < 2){
+        return a
     }
 
-    return  a *  factorial(a-1)
+    return fibonacci(a-1) + fibonacci(a-2)
 }
 
-// console.log(factorial(5))
+// console.log(fibonacci(7))
